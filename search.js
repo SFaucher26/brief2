6de28cc2fe56f94729cd6d6f9c0d599bb843b6fe
SFaucher26 <sandrine.faucher@maif.fr>
2024-02-22 11:38:01 +0100

    changement des noms d'images des chevaliers

--- search.js
+++ search.js
@@ -13,28 +13,28 @@ let chevaliers = [
     nom: "Shiryu du Dragon",
     description:
       "Shiryu est le chevalier de Bronze du Dragon. Il a effectué son entrainement et obtenu son armure en Chine. Profondément épris de justice qu’il considère comme quelque chose d’absolu, il n’hésite jamais à mettre sa vie en jeu pour la défendre.",
-    imgUrl: "./img/shiryu.webp",
+    imgUrl: "./img/shiryu2.webp",
     imgAlt: "image de Shiryu",
   },
   {
     nom: "Hyôga du Cygne",
     description:
       "Hyôga a effectué son entrainement en Sibérie. Cachant un douloureux passé, il décide de devenir le chevalier de Bronze du Cygne pour acquérir la force nécessaire, afin de pouvoir plonger dans l’eau glacée et se rendre sur le tombeau de sa mère. Cette dernière repose dans une épave au fond de la mer de glace, suite au naufrage du bateau les conduisant au Japon.",
-    imgUrl: "./img/hyoga.webp",
+    imgUrl: "./img/hyoga2.webp",
     imgAlt: "image de Hyôga",
   },
   {
     nom: "Shun d’Andromède",
     description:
       "Shun est le chevalier de Bronze d’Andromède et le frère d’Ikki. Malgré un tempérament calme et foncièrement pacifiste, il serait le plus puissant des chevaliers de Bronze. Il déteste se battre et tente toujours de raisonner son ennemi avant de recourir à la force.",
-    imgUrl: "./img/shun.webp",
+    imgUrl: "./img/shun2.webp",
     imgAlt: "image de Shun",
   },
   {
     nom: "Ikki du Phoenix",
     description:
       "Ikki est un cas un peu particulier. Chevalier de Bronze du Phénix, il est d’abord un ennemi qui cherche à s’emparer de l’armure d’Or du Sagittaire et à conquérir le monde. Il se rallie à Athéna en se rendant compte de sa méprise. Ne faisant pas réellement partie du « noyau dur » des chevaliers de Bronze, il apparait quand bon lui semble et est indépendant, comparé aux autres.",
-    imgUrl: "./img/ikki.webp",
+    imgUrl: "./img/ikki2.webp",
     imgAlt: "image de Ikki",
   },
 ];
